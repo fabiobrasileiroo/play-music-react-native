69e42244b692636a539b6e71065e9b2ece3b9707
Fabio Brasileiro <fabiotrocados@debian-BULLSEYE-live-builder-AMD64>
2024-02-17 11:18:48 -0400

    ajustando componentes

--- src/MusicPlayer/MusicPlayer.js
+++ src/MusicPlayer/MusicPlayer.js
@@ -5,16 +5,32 @@ import {
   StyleSheet,
   SafeAreaView,
   TouchableOpacity,
+  Dimensions,
+  Image,
 } from "react-native";
 import Ionicons from "@expo/vector-icons/Ionicons";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+// import { Entypo } from '@expo/vector-icons';
+
+const { width, height } = Dimensions.get("window");
 
 const MusicPlayer = () => {
   return (
     <SafeAreaView style={style.container}>
       <View style={style.maincontainer}>
         {/* image */}
+        <View style={[style.imageWrapper, style.elevation]}>
+          <Image
+            source={require("../../assets/img/img2.png")}
+            style={style.musicImage}
+          />
+        </View>
         {/* slider */}
         {/* music controls */}
+        <View>
+          <Text style={style.songTitle}>Some Title</Text>
+          <Text style={style.songArtist}>Some Artist Name</Text>
+        </View>
       </View>
       <View style={style.bottomContainer}>
         <View style={style.bottomIconWrapper}>
@@ -22,13 +38,18 @@ const MusicPlayer = () => {
             <Ionicons name="heart-outline" size={32} color="#888888" />
           </TouchableOpacity>
           <TouchableOpacity onPress={() => {}}>
-            <Ionicons name="heart-outline" size={32} color="#888888" />
+            <Ionicons name="repeat" size={32} color="#888888" />
           </TouchableOpacity>
           <TouchableOpacity onPress={() => {}}>
-            <Ionicons name="heart-outline" size={32} color="#888888" />
+            <Ionicons name="share-outline" size={32} color="#888888" />
           </TouchableOpacity>
           <TouchableOpacity onPress={() => {}}>
-            <Ionicons name="heart-outline" size={32} color="#888888" />
+            <MaterialCommunityIcons
+              name="dots-horizontal"
+              size={32}
+              color="#888888"
+            />
+            {/* <Entypo name="dots-three-horizontal" size={32} color="#888888" /> */}
           </TouchableOpacity>
         </View>
       </View>
@@ -47,10 +68,47 @@ const style = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
-  bottomContainer: {},
+  bottomContainer: {
+    width: width,
+    alignItems: "center",
+    paddingVertical: 15,
+    borderTopColor: "#393E46",
+    borderWidth: 1,
+  },
   bottomIconWrapper: {
     flexDirection: "row",
-    justifyContent: 'space-between',
-    width: '80%'
+    justifyContent: "space-between",
+    width: "80%",
+  },
+  imageWrapper: {
+    width: 300,
+    height: 340,
+    marginBottom: 25,
+  },
+  musicImage: {
+    width: "100%",
+    height: "100%",
+    borderRadius: 15,
+  },
+  elevation: {
+    shadowColor: "#ccc",
+    shadowOffset: {
+      width: 5,
+      height: 5,
+    },
+    shadowOpacity: 0.5,
+    shadowRadius: 3.84,
+  },
+  songTitle: {
+    fontSize: 18,
+    fontWeight: "600",
+    textAlign: "center",
+    color: "#EEEEEE",
+  },
+  songArtist: {
+    fontSize: 16,
+    fontWeight: "300",
+    textAlign: "center",
+    color: "#EEEEEE",
   },
 });
